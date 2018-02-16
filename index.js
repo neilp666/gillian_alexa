24217e60727dd70e9e918545fe4f5cf865a8bd38
neil patel <neil@myindianweddingapp.com>
2018-02-16 17:33:31 +0000

    Get random author if undefined

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var Alexa = require('alexa-sdk');
 
+
+var QuoteDataObj = {
+                    Lincoln: [
+                      "Whatever you are be a good one."
+                    ],
+                    Einstein: [
+                      "Imagination is more important than knowledge",
+                      "If the fact don't fit the theory, change the facts.",
+                      "Life is like a riding a bicycle. To keep you balance you must keep moving"
+                    ]
+};
+
+
+// a separate function that takes out the quote and returns the author and quote
+
+function getQuoteFunction(author) {
+
+  console.log("Getting into getQuoteFunction");
+
+  // Get random author if author is not defined
+  if (author === undefined) {
+    var totalauthors = Object.keys(QuoteDataObj).length;
+    var rand = Math.floor(Math.random() * totalauthors);
+
+    // random author name
+    author = Object.keys(QuoteDataObj)[rand];
+  }
+
+}
+
+
 var handlers = {
 
     // When launched without any action
